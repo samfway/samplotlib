dd1011d9e165762a2c85e976aa0a16c1b5f45e4e
Sam Way <samfway@cu-vpn-colorado-edu-198.11.28.209.int.colorado.edu>
2017-02-25 16:33:20 -0700

    Initial commit of constants, helper functions

---
+++ samplotlib/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+__author__ = "Sam Way"
+__copyright__ = "Copyright 2017, The Clauset Lab"
+__license__ = "BSD"
+__maintainer__ = "Sam Way"
+__email__ = "samfway@gmail.com"
+__status__ = "Development"
+
+
